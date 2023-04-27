04008f5deceb610d623a17240eeaf502629b2d9e
Patryk <bazior.patryk@gmail.com>
2023-04-27 06:15:51 +0200

    form validation

--- src/components/features/PostForm.js
+++ src/components/features/PostForm.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import TextInput from '../common/TextInput/TextInput';
 import { InputGroup } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useForm } from 'react-hook-form';
 
 const PostForm = ({ action, actionText, ...props }) => {
   const [title, setTitle] = useState(props.title || '');
@@ -12,10 +15,23 @@ const PostForm = ({ action, actionText, ...props }) => {
   const [date, setDate] = useState(new Date());
   const [description, setDescription] = useState(props.description || '');
   const [content, setContent] = useState(props.content || '');
-  const handleForm = (e) => {
-    e.preventDefault();
-    action({ title, author, publishedDate: date, shortDescription: description, content });
+  const [dateError, setDateError] = useState(false);
+  const [contentError, setContentError] = useState(false);
+
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
+
+  const handleForm = () => {
+    setContentError(!content);
+    setDateError(!date);
+    if (content && date) {
+      action({ title, author, publishedDate: date, shortDescription: description, content });
+    }
   };
+
   function handleUserInputChange(newUserValue) {
     const diff = newUserValue;
     setContent(diff);
@@ -24,34 +40,67 @@ const PostForm = ({ action, actionText, ...props }) => {
   return (
     <>
       <form>
-        <InputGroup className="w-50">
-          <TextInput title="Title" placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <TextInput
-            title="Author"
-            placeholder="Enter author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+        <InputGroup className="w-50 flex-column">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              {...register('title', { required: true, minLength: 3 })}
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              placeholder="Enter title"
+            />
+            {errors.title && (
+              <small className="d-block form-text text-danger mt-2">
+                This field is required ( minimum 3 characters )
+              </small>
+            )}
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Author</Form.Label>
+            <Form.Control
+              {...register('author', { required: true, minLength: 3 })}
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              type="text"
+              placeholder="Enter author"
+            />
+            {errors.author && (
+              <small className="d-block form-text text-danger mt-2">
+                This field is required ( minimum 3 characters )
+              </small>
+            )}
+          </Form.Group>
         </InputGroup>
-        <DatePicker selected={date} onChange={(date) => setDate(date)} dateFormat="dd/MM/yyyy" />
-        <InputGroup className="mb-4 ">
-          <TextInput
-            title="Description"
-            placeholder="Leave a comment here"
-            as="textarea"
-            rows={5}
+        <Form.Group className="mb-4" controlId="formBasicEmail">
+          <Form.Label>Date</Form.Label>
+          <DatePicker selected={date} onChange={(date) => setDate(date)} dateFormat="dd/MM/yyyy" />
+          {dateError && <small className="d-block form-text text-danger">Choose date!</small>}
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="formBasicEmail">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            className="w-100 mb-2"
+            {...register('description', { required: true, minLength: 20 })}
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            type="textarea"
+            placeholder="Enter description"
+            rows={10}
           />
-        </InputGroup>
-        <ReactQuill
-          className="mb-4"
-          placeholder="Leave a comment here"
-          value={content}
-          onChange={handleUserInputChange}
-        />
+          {errors.description && (
+            <small className="d-block form-text text-danger mt-2">
+              This field is required ( minimum 20 characters )
+            </small>
+          )}
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="formBasicEmail">
+          <Form.Label>Content</Form.Label>
+          <ReactQuill placeholder="Leave a comment here" value={content} onChange={handleUserInputChange} />
+          {contentError && <small className="d-block form-text text-danger">Content can't be empty</small>}
+        </Form.Group>
       </form>
-      <button className="btn btn-primary" onClick={handleForm}>
+      <button className="btn btn-primary" onClick={validate(handleForm)}>
         {actionText}
       </button>
     </>
