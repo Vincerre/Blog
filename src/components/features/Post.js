63e920357e25f7fe49612ccbc7512ff2ed413995
Patryk Vincerre <bazior.patryk@gmail.com>
2023-04-27 11:02:47 +0200

    commit

---
+++ src/components/features/Post.js
@@ -0,0 +1,28 @@
+import { getPosts } from '../../redux/postsRedux';
+import { useSelector } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+const Post = () => {
+  const posts = useSelector(getPosts);
+
+  return (
+    <section className="d-flex">
+      {posts.map((post) => (
+        <div key={post.id} className="p-3 ">
+          <h3>{post.title}</h3>
+          <p>
+            <b>Author:</b>
+            {post.author}
+          </p>
+          <p>
+            <b>Published:</b>
+            {post.publishedDate}
+          </p>
+          <p>{post.shortDescription}</p>
+          <Button variant="primary">Read More</Button>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
